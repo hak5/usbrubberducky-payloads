3764def08d3275e14ad43859c38178c1725d954d
Rayden <66985140+MocconaCoffee@users.noreply.github.com>
2023-05-10 04:30:53 +1000

    Commit Horsd payload

---
+++ payloads/library/prank/Horsd/horsd.ps1
@@ -0,0 +1,18 @@
+$Image="%UserProfile%/horse.jpg"
+$code = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{ 
+    
+     public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
+
+add-type $code 
+[Win32.Wallpaper]::SetWallpaper($Image)
