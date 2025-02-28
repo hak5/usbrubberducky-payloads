9fa8e0861c685345c0ad41b0a3e6d1b041d9cb3d
gavhu10 <gavinhutten@gmail.com>
2025-02-27 20:45:57 -0500

    Update payload.ps1 to take a screen shot

--- payloads/library/prank/Unclickable-Icons/payload.ps1
+++ payloads/library/prank/Unclickable-Icons/payload.ps1
@@ -1,2 +1,20 @@
-# HelloWorld.ps1
-Write-Host "Hello, World!"
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+# Create a bitmap of the size of the screen
+$screen = [System.Windows.Forms.Screen]::PrimaryScreen
+$bitmap = New-Object System.Drawing.Bitmap $screen.Bounds.Width, $screen.Bounds.Height
+
+# Create a graphics object from the bitmap
+$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+
+# Take the screenshot
+$graphics.CopyFromScreen($screen.Bounds.Location, [System.Drawing.Point]::Empty, $screen.Bounds.Size)
+
+# Save the screenshot to a file
+$savePath = "./DesktopScreenshot.png"  # Change this to your desired path
+$bitmap.Save($savePath, [System.Drawing.Imaging.ImageFormat]::Png)
+
+# Clean up
+$graphics.Dispose()
+$bitmap.Dispose()
