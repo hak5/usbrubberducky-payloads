d98ac85af3bf9793519fead934dd2fdaf945d678
gavhu10 <gavinhutten@gmail.com>
2025-02-27 20:54:25 -0500

    Update payload.ps1

--- payloads/library/prank/Unclickable-Icons/payload.ps1
+++ payloads/library/prank/Unclickable-Icons/payload.ps1
@@ -1,18 +1,32 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-# Create a bitmap of the size of the screen
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen
-$bitmap = New-Object System.Drawing.Bitmap $screen.Bounds.Width, $screen.Bounds.Height
+# Create a bitmap that covers all screens
+$allScreens = [System.Windows.Forms.Screen]::AllScreens
+$width = 0
+$height = 0
+
+# Calculate the total width and height of all screens
+foreach ($screen in $allScreens) {
+    $width += $screen.Bounds.Width
+    $height = [Math]::Max($height, $screen.Bounds.Height)
+}
+
+# Create a bitmap of the total size
+$bitmap = New-Object System.Drawing.Bitmap $width, $height
 
 # Create a graphics object from the bitmap
 $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
 
-# Take the screenshot
-$graphics.CopyFromScreen($screen.Bounds.Location, [System.Drawing.Point]::Empty, $screen.Bounds.Size)
+# Capture each screen
+$xOffset = 0
+foreach ($screen in $allScreens) {
+    $graphics.CopyFromScreen($screen.Bounds.Location, [System.Drawing.Point]::new($xOffset, 0), $screen.Bounds.Size)
+    $xOffset += $screen.Bounds.Width
+}
 
 # Save the screenshot to a file
-$savePath = "./DesktopScreenshot.png"  # Change this to your desired path
+$savePath = "./DesktopScreenShot.png"  # Change this to your desired path
 $bitmap.Save($savePath, [System.Drawing.Imaging.ImageFormat]::Png)
 
 # Clean up
