c2bad72409cc62d20434b6b625471e50a618ebf5
gavhu10 <gavinhutten@gmail.com>
2025-02-27 20:56:59 -0500

    Update payload.ps1 to take a screen shot v3

--- payloads/library/prank/Unclickable-Icons/payload.ps1
+++ payloads/library/prank/Unclickable-Icons/payload.ps1
@@ -1,34 +1,16 @@
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-
-# Create a bitmap that covers all screens
-$allScreens = [System.Windows.Forms.Screen]::AllScreens
-$width = 0
-$height = 0
-
-# Calculate the total width and height of all screens
-foreach ($screen in $allScreens) {
-    $width += $screen.Bounds.Width
-    $height = [Math]::Max($height, $screen.Bounds.Height)
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+   
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+   
+   $bmp.Save($path)
+   
+   $graphics.Dispose()
+   $bmp.Dispose()
 }
 
-# Create a bitmap of the total size
-$bitmap = New-Object System.Drawing.Bitmap $width, $height
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1000, 900)
+screenshot $bounds "./DesktopScreenShot.png" 
 
-# Create a graphics object from the bitmap
-$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-
-# Capture each screen
-$xOffset = 0
-foreach ($screen in $allScreens) {
-    $graphics.CopyFromScreen($screen.Bounds.Location, [System.Drawing.Point]::new($xOffset, 0), $screen.Bounds.Size)
-    $xOffset += $screen.Bounds.Width
-}
-
-# Save the screenshot to a file
-$savePath = "./DesktopScreenShot.png"  # Change this to your desired path
-$bitmap.Save($savePath, [System.Drawing.Imaging.ImageFormat]::Png)
-
-# Clean up
-$graphics.Dispose()
-$bitmap.Dispose()
