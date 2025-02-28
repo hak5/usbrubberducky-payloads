696fb4d97fe1d98cbe10a1975d726fa5e6812e8f
gavhu10 <gavinhutten@gmail.com>
2025-02-27 21:23:39 -0500

    Update payload.ps1

--- payloads/library/prank/Unclickable-Icons/payload.ps1
+++ payloads/library/prank/Unclickable-Icons/payload.ps1
@@ -1,22 +1,9 @@
-# Define the function to take a screenshot
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-# Create a bitmap object with the size of the screen
-$bitmap = New-Object System.Drawing.Bitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
+$bounds = [Windows.Forms.SystemInformation]::VirtualScreen
+$screenshot = [Drawing.Bitmap]::new($bounds.Width, $bounds.Height)
+$graph = [Drawing.Graphics]::FromImage($screenshot)
+$graph.CopyFromScreen($bounds.X, $bounds.Y, 0, 0, $bounds.Size)
+$screenshot.Save(".\screenshot.png")
 
-# Create a graphics object from the bitmap
-$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-
-# Capture the screen
-$graphics.CopyFromScreen(0, 0, 0, 0, $bitmap.Size)
-
-# Save the screenshot to a file
-$screenshotPath = "./DesktopScreenshot"
-$bitmap.Save($screenshotPath, [System.Drawing.Imaging.ImageFormat]::Png)
-
-# Clean up
-$graphics.Dispose()
-$bitmap.Dispose()
-
-Write-Host "Screenshot saved to $screenshotPath"
