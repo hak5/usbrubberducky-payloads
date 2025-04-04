1de9e9b15242d840beae60b844b8afcdb9f3890a
gavhu10 <gavinhutten@gmail.com>
2025-04-04 13:56:00 -0400

    Delete payloads/library/prank/Unclickable-Icons directory

--- payloads/library/prank/Unclickable-Icons/payload.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-
-$bounds = [Windows.Forms.SystemInformation]::VirtualScreen
-$screenshot = [Drawing.Bitmap]::new($bounds.Width, $bounds.Height)
-$graph = [Drawing.Graphics]::FromImage($screenshot)
-$graph.CopyFromScreen($bounds.X, $bounds.Y, 0, 0, $bounds.Size)
-$screenshot.Save(".\screenshot.png")
-
